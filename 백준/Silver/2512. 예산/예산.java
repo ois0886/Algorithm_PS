1f5a04e500cf80f062cbc1eb328ebd456ab34ed9
oh in seong <ohinsung64@gmail.com>
2023-11-26 18:01:13 +0900

    [Silver II] Title: 예산, Time: 176 ms, Memory: 15208 KB -BaekjoonHub

---
+++ 백준/Silver/2512. 예산/예산.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int left = 0, right = -1;
+        int[] arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            right = Math.max(right, arr[i]);
+        }
+
+        int m = Integer.parseInt(br.readLine());
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            long budget = 0;
+            for (int i = 0; i < n; i++) {
+                budget += Math.min(arr[i], mid);
+            }
+            if (budget <= m) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        System.out.println(right);
+    }
+}
