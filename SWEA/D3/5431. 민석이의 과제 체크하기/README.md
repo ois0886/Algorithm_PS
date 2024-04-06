b97f346b2a1c9dfcc9ed6b21d3522562cf52772e
oh in seong <ohinsung64@gmail.com>
2024-04-06 20:37:27 +0900

    [D3] Title: 민석이의 과제 체크하기, Time: 218 ms, Memory: 71,676 KB -BaekjoonHub

---
+++ SWEA/D3/5431. 민석이의 과제 체크하기/README.md
@@ -0,0 +1,15 @@
+# [D3] 민석이의 과제 체크하기 - 5431 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWVl3rWKDBYDFAXm) 
+
+### 성능 요약
+
+메모리: 71,676 KB, 시간: 218 ms, 코드길이: 726 Bytes
+
+### 제출 일자
+
+2024-04-06 20:37
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D3/5431. 민석이의 과제 체크하기/민석이의 과제 체크하기.java
@@ -0,0 +1,31 @@
+import java.util.*;
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+        StringBuffer sb = new StringBuffer();
+
+        int T = sc.nextInt();
+        for(int tc=1; tc<=T; tc++){
+            sb.append("#").append(tc).append(" ");
+
+            int N = sc.nextInt();
+            boolean student[] = new boolean[N+1];
+
+            int K = sc.nextInt();
+            for(int i=0; i<K; i++){
+                student[sc.nextInt()] = true;
+            }
+
+            for(int i=1; i<=N; i++){
+                if(!student[i])
+                    sb.append(i).append(" ");
+            }
+            sb.append("\n");
+
+        }
+        System.out.println(sb);
+
+    }
+}
