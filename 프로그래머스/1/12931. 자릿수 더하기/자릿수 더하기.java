64ac94610564dbb6da9c6ec6b2f267b1f545d7e7
oh in seong <ohinsung64@gmail.com>
2024-04-11 15:54:58 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 80.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        while (n > 0) {
+            int temp = n % 10;
+            answer += temp;
+            n /= 10;
+        }
+
+
+        return answer;
+    }
+}
