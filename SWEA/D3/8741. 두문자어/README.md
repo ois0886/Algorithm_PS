6f24c8258f3f4079908d7763221c060c0603f043
oh in seong <ohinsung64@gmail.com>
2024-04-16 14:45:53 +0900

    [D3] Title: 두문자어, Time: 123 ms, Memory: 25,240 KB -BaekjoonHub

---
+++ SWEA/D3/8741. 두문자어/README.md
@@ -0,0 +1,15 @@
+# [D3] 두문자어 - 8741 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AW2y6n3qPXQDFATy) 
+
+### 성능 요약
+
+메모리: 25,240 KB, 시간: 123 ms, 코드길이: 669 Bytes
+
+### 제출 일자
+
+2024-04-16 14:45
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D3/8741. 두문자어/두문자어.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        for (int test_case = 1; test_case <= T; test_case++) {
+            sb.append("#").append(test_case).append(" ");
+            String[] str = br.readLine().split(" ");
+            for (String s : str) {
+                sb.append(String.valueOf(s.charAt(0)).toUpperCase());
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+}
