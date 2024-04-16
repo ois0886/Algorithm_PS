b5817ffc53c3c31a75ce13fe44d9d4730c71eba1
oh in seong <ohinsung64@gmail.com>
2024-04-16 16:13:23 +0900

    [D3] Title: 최대 성적표 만들기, Time: 170 ms, Memory: 48,852 KB -BaekjoonHub

---
+++ SWEA/D3/4466. 최대 성적표 만들기/README.md
@@ -0,0 +1,15 @@
+# [D3] 최대 성적표 만들기 - 4466 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWOUfCJ6qVMDFAWg) 
+
+### 성능 요약
+
+메모리: 48,852 KB, 시간: 170 ms, 코드길이: 781 Bytes
+
+### 제출 일자
+
+2024-04-16 16:13
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D3/4466. 최대 성적표 만들기/최대 성적표 만들기.java
@@ -0,0 +1,31 @@
+import java.util.*;
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		        Scanner sc = new Scanner(System.in);
+        StringBuffer sb = new StringBuffer();
+
+
+        int T = sc.nextInt();
+        for (int tc = 1; tc <= T; tc++) {
+            sb.append("#").append(tc).append(" ");
+
+            int N = sc.nextInt();
+            int K = sc.nextInt();
+
+            Integer array[] = new Integer[N];
+            for(int i=0; i<N; i++){
+                array[i] = sc.nextInt();
+            }
+            Arrays.sort(array, Collections.reverseOrder());
+
+            int sum = 0;
+            for(int i=0; i<K; i++){
+                sum += array[i];
+            }
+            sb.append(sum).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
