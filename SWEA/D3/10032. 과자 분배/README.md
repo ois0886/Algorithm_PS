80665eafc6d7d60793eb69231809ad4e856b3090
oh in seong <ohinsung64@gmail.com>
2024-04-24 16:31:40 +0900

    [D3] Title: 과자 분배, Time: 189 ms, Memory: 43,096 KB -BaekjoonHub

---
+++ SWEA/D3/10032. 과자 분배/README.md
@@ -0,0 +1,15 @@
+# [D3] 과자 분배 - 10032 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AXJZ6_6KCLcDFAU3) 
+
+### 성능 요약
+
+메모리: 43,096 KB, 시간: 189 ms, 코드길이: 609 Bytes
+
+### 제출 일자
+
+2024-04-24 16:31
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D3/10032. 과자 분배/과자 분배.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        for (int test_case = 1; test_case <= T; test_case++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int K = Integer.parseInt(st.nextToken());
+            int res = N % K > 0 ? 1 : 0;
+            System.out.println("#" + test_case + " " + res);
+        }
+    }
+}
