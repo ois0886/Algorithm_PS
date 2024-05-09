47a77fa58dc5b4fdcf9c7715d5e120847a14af13
oh in seong <ohinsung64@gmail.com>
2024-05-09 16:44:01 +0900

    [D3] Title: 사랑의 카운슬러, Time: 308 ms, Memory: 26,856 KB -BaekjoonHub

---
+++ SWEA/D3/7227. 사랑의 카운슬러/README.md
@@ -0,0 +1,15 @@
+# [D3] 사랑의 카운슬러 - 7227 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWlQUD2qtysDFAVS) 
+
+### 성능 요약
+
+메모리: 26,856 KB, 시간: 308 ms, 코드길이: 1,795 Bytes
+
+### 제출 일자
+
+2024-05-09 16:43
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D3/7227. 사랑의 카운슬러/사랑의 카운슬러.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    public static int N;
+    public static long answer;
+    public static int[] number;
+    public static int[][] earthworms;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int T = Integer.parseInt(br.readLine());
+        for (int tc = 1; tc <= T; ++tc) {
+            answer = Long.MAX_VALUE;
+
+            N = Integer.parseInt(br.readLine());
+
+            earthworms = new int[N][2];
+            number = new int[N];
+
+            for (int i = 0; i < N; ++i) {
+                st = new StringTokenizer(br.readLine(), " ");
+
+                earthworms[i][0] = Integer.parseInt(st.nextToken());
+                earthworms[i][1] = Integer.parseInt(st.nextToken());
+            }
+
+            combination(0, 0);
+
+            sb.append("#").append(tc).append(" ").append(answer).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    public static void combination(int cnt, int start) {
+        if (cnt == N / 2) {
+            long dx = 0, dy = 0;
+            for (int i = 0; i < N; ++i) {
+                if (number[i] > 0) {
+                    dx += earthworms[i][0];
+                    dy += earthworms[i][1];
+                } else {
+                    dx -= earthworms[i][0];
+                    dy -= earthworms[i][1];
+                }
+            }
+            long vectorSize = (dx * dx) + (dy * dy);
+
+            answer = Math.min(answer, vectorSize);
+            return;
+        }
+
+        for (int i = start; i < N; ++i) {
+            number[i] = 1;
+            combination(cnt + 1, i + 1);
+            number[i] = 0;
+        }
+    }
+}
