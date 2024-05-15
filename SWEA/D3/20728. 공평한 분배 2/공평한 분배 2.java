ae3df7ab507e4abf2c6a027afece4d5575e845fe
oh in seong <ohinsung64@gmail.com>
2024-05-15 16:51:39 +0900

    [D3] Title: 공평한 분배 2, Time: 121 ms, Memory: 22,048 KB -BaekjoonHub

---
+++ SWEA/D3/20728. 공평한 분배 2/공평한 분배 2.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        for (int test_case = 1; test_case <= T; test_case++) {
+            sb.append("#").append(test_case).append(" ");
+            st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int K = Integer.parseInt(st.nextToken());
+            int[] arr = new int[N];
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < N; i++) {
+                arr[i] = Integer.parseInt(st.nextToken());
+            }
+            Arrays.sort(arr);
+            int answer = Integer.MAX_VALUE;
+            for(int i = 0; i < N-K+1; i++){
+                int diff = arr[i+K-1]-arr[i];
+                answer = Math.min(answer, diff);
+            }
+            sb.append(answer).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
