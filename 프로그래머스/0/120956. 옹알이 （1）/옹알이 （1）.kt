8fc56e23c72482f6f7d657c55a7d49d218df4369
oh in seong <ohinsung64@gmail.com>
2024-06-17 16:22:18 +0900

    [level 0] Title: 옹알이 (1), Time: 33.02 ms, Memory: 65.9 MB -BaekjoonHub

--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.kt
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.kt
@@ -1,11 +1,6 @@
 class Solution {
     fun solution(babbling: Array<String>): Int {
-        val count : Int
-        count = babbling.count {
-            it.isNotEmpty() && it.split("aya","ye","woo","ma").none {
-                it.isNotEmpty()
-            }
-        }
-        return count
+        var answer: Int = 0
+        return answer
     }
 }
