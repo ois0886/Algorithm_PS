9dd03715445cb945015344b04617ffe44d6b045f
oh in seong <ohinsung64@gmail.com>
2024-11-12 15:41:14 +0900

    [Silver I] Title: 카드 합체 놀이, Time: 148 ms, Memory: 15248 KB -BaekjoonHub

---
+++ 백준/Silver/15903. 카드 합체 놀이/카드 합체 놀이.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+import java.math.*;
+public class Main {
+    static PriorityQueue<Long>pq = new PriorityQueue<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String [] t = br.readLine().split(" ");
+        
+        int n = Integer.parseInt(t[0]);
+        int m = Integer.parseInt(t[1]);
+            
+        String [] tt = br.readLine().split(" ");
+        
+        for(int i=0; i<n; i++) {
+            pq.add(Long.parseLong(tt[i]));
+        }
+        for(int i=0; i<m; i++) {
+            Long a = pq.poll();
+            Long b = pq.poll();
+            pq.add(a+b);
+            pq.add(a+b);
+        }
+    
+        long ans = 0;
+        
+        
+        while(!pq.isEmpty()) {
+            ans+=pq.poll();
+        }
+        System.out.println(ans);
+    }
+}
