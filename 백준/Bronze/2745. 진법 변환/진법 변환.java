71ccba3df628345889f0fc016708cb3fcce455b3
oh in seong <ohinsung64@gmail.com>
2025-01-18 14:55:58 +0900

    [Bronze II] Title: 진법 변환, Time: 104 ms, Memory: 14376 KB -BaekjoonHub

---
+++ 백준/Bronze/2745. 진법 변환/진법 변환.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        String N = st.nextToken();
+        int B = Integer.parseInt(st.nextToken());
+        System.out.println(Integer.parseInt(N, B));
+
+    }
+}
