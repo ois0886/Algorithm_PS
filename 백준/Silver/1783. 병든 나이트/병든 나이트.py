77844011a8a0fe0f879576c6bdd24e597d51f247
oh in seong <ohinsung64@gmail.com>
2026-02-26 09:28:07 +0900

    [Silver III] Title: 병든 나이트, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1783. 병든 나이트/병든 나이트.py
@@ -0,0 +1,15 @@
+import sys
+
+input = sys.stdin.readline
+def solve():
+    N, M = map(int, input().split())
+    if N == 1:
+        print(1)
+    elif N == 2:
+        print(min(4, (M - 1) // 2 + 1))
+    elif M < 7:
+        print(min(4, M))
+    else:
+        print(M - 2)
+
+solve()
